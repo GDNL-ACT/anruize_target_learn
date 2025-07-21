c6da96dbb499afaaf5719b693570f23db07a14cb
ruize.an <ruize.an@outlook.com>
2025-07-21 11:04:47 +0000

    anantest

---
+++ script/llama_llara_first_last_train.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Set the NCCL timeout to 600000 milliseconds (10 minutes)
+export NCCL_TIMEOUT=60000
+export NCCL_DEBUG=INFO
+# export NCCL_P2P_DISABLE=1
+# export NCCL_IB_DISABLE=1
+export CUDA_LAUNCH_BLOCKING=1
+export TORCH_NCCL_BLOCKING_WAIT=1
+export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
+
+MODEL_NAME="llama3-8b"
+BASE_MODEL_PATH="/data/LLM/llama3/llama3-8b-instruct"
+FIRST_MAX_LENGTH=128
+
+PORT=29600  # 你可以根据实际需要修改端口号
+accelerate launch \
+    --main_process_port $PORT \
+    --num_cpu_threads_per_process 1 \
+    aa_gene_train.py \
+    --model_name $MODEL_NAME \
+    --base_model_name_or_path $BASE_MODEL_PATH \
+    --lora_r 16 \
+    --lora_alpha 32 \
+    --target_modules q_proj, v_proj, k_proj, o_proj, gate_proj, up_proj, down_proj \
+    --lora_dropout 0.05 \
+    --bias none \
+    --prompt 'This_sentence_:_"*sent_0*"_means_in_one_word:"' \
+    --learning_rate 5e-5 \
+    --per_device_train_batch_size 32 \
+    --gradient_accumulation_steps 1 \
+    --save_steps 5 \
+    --max_train_steps 100 \
+    --mode llara_first_last \
+    --pooling_mode last \
+    --output_dir learn_from_target \
+    --max_length $FIRST_MAX_LENGTH \
+    --dataset_path 'cfli/pretrain_wiki'
